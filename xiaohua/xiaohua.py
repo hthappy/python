510a2f13730cb58587542c104b0c55717f212bbf
hthappy <tivon.he@qq.com>
2017-09-20 11:55:38 +0800

    优化
    优化

--- xiaohua/xiaohua.py
+++ xiaohua/xiaohua.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 import urllib
 import re
+import os
 
 
 
@@ -13,22 +14,16 @@ def getList(page):
     return picurl
 
 def images(picurl):
-    if len(picurl) > 20: #检查无效地址
-        html = urllib.urlopen(picurl).read().decode('gbk', 'ignore').encode('utf-8', 'ignore')
-        reg = r'<a target="_blank" class="imglink" href="(.*?)"><img alt='
-        reg = re.compile(reg)
-        imglink = re.findall(reg,html)
-        if str(imglink) == '[]':
-            pass
-        else:
-            imglink = imglink[0]
-            html = urllib.urlopen(imglink).read()
-            reg = r'class="inner"><a href="(.*?)" class="">'
-            reg = re.compile(reg)
-            imgurl = re.findall(reg,html)
-        return imglink
-    else:
-        return None
+    html = urllib.urlopen(picurl).read().decode('gbk', 'ignore').encode('utf-8', 'ignore')
+    reg = r'<a target="_blank" class="imglink" href="(.*?)"><img alt='
+    reg = re.compile(reg)
+    imglink = re.findall(reg,html)
+    html = urllib.urlopen(imglink[0]).read().decode('gbk', 'ignore').encode('utf-8', 'ignore')
+    reg = r'class="inner"><a href="(.*?)" class="">'
+    reg = re.compile(reg)
+    imgurl = re.findall(reg,html)
+    return imgurl
+
 
 
 
@@ -39,18 +34,18 @@ for i in range(0,44):
     print '###################'
     for picurl,title in getList(page=i): #获取相册地址和相册名
         num = 1
-        if images(picurl) != None :
-            for imgurl in images(picurl=picurl): #获取相册集
-                if imgurl != []:
-                    imgurls = 'http://www.xiaohuar.com%s' %imgurl #照片完整地址
-                    imgdir = '/Users/tao/Pictures/校花/%s' % title
-                    urllib.urlretrieve(imgurls, imgdir + '%s.jpg' % num)
-                    print '%s 保存成功' % title
-                    num += 1
-                else:
-                    print '未获取到 %s 图片地址' %title
-        else:
-            print '%s 地址无效 - 跳过' %picurl
+        for imgurl in images(picurl=picurl): #获取相册集
+            imgurls = 'http://www.xiaohuar.com%s' %imgurl #照片完整地址
+            imgdir = '/Users/tao/Pictures/校花/%s/' % title #保存目录
+            if os.path.isdir(imgdir):
+                urllib.urlretrieve(imgurls, imgdir + '%s.jpg' % num)
+                print '%s 保存成功' % title
+                num += 1
+            else:
+                os.makedirs(imgdir)
+                urllib.urlretrieve(imgurls, imgdir + '%s.jpg' % num)
+                print '%s 保存成功' % title
+                num += 1
 
 
 
